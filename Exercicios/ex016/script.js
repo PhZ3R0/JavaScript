0e5df679843358effdf7ea61ce43f4741496fcce
Pablo Henrique <105991077+PhZ3R0@users.noreply.github.com>
2025-09-25 07:44:57 -0300

    Update script.js

--- Exercicios/ex016/script.js
+++ Exercicios/ex016/script.js
@@ -4,15 +4,28 @@ function contar() {
     let passo = document.getElementById('txtp')
     let res = document.getElementById('res')
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        alert('Erro, faltam informações!')
+        res.innerHTML += 'Impossível contar!'
+        // alert('Erro, faltam informações!')
     }   else {
             let i = Number(ini.value)        
             let f = Number(fim.value)      
-            let p = Number(passo.value)       
-
-            for (let c = i; c<=f;c+=p) {
-                res.innerHTML += 'Testando'
+            let p = Number(passo.value)  
+            if (p<=0) {
+                alert('Passo inváldo considerando passo = 1')
+                p = 1
+            }     
+            if (i<f) {
+                // contagem crescente
+                for (let c = i; c<=f;c+=p) {
+                    res.innerHTML += ` ${c} \u{1f449}`
+                }   
+                // contagem decrescente
+            }   else {
+                for (let c = i; c >=f; c-=p) {
+                        res.innerHTML += ` ${c} \u{1f449}`
+                    }
             }
+            res.innerHTML+=` \u{1f3c1}`
         }
     }
   
