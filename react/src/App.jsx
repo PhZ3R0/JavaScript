9cd103aff6d9c1fc56c28cf942ca33ce005f28e7
Pablo Henrique <105991077+PhZ3R0@users.noreply.github.com>
2025-09-26 16:06:37 -0300

    a

---
+++ react/src/App.jsx
@@ -0,0 +1,16 @@
+import AddTask from "./components/AddTask";
+import Tasks from "./components/tasks";
+
+
+function App() {
+
+  return (
+    <div>
+      <h1 className="text-red-500" >Gerenciador de Tarefas</h1>
+      <AddTask />
+      <Tasks />
+    </div>
+  );
+}
+
+export default App;
